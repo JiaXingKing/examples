5ca69b14baad44bf21eb71d315d29cd132fb5b62
JiaXingKing <tomcheng0573@gmail.com>
2019-10-08 16:27:46 +0800

    用户登录和Helper

--- sequelize/app/controller/systemConfig/sys_user.js
+++ sequelize/app/controller/systemConfig/sys_user.js
@@ -1,5 +1,5 @@
 'use strict';
-
+const jwt = require("jsonwebtoken"); // 生成token
 const Controller = require('egg').Controller;
 
 class UserController extends Controller {
@@ -36,6 +36,21 @@ class UserController extends Controller {
     const id = ctx.helper.parseInt(ctx.params.id);
     await ctx.service.systemConfig.sysUser.delete(id);
     ctx.status = 200;
+  }
+  async login() {
+    const ctx = this.ctx;
+    const accountName = ctx.query.accountName;
+    const passWord = ctx.query.passWord;
+    if(!ctx.helper.checkDataIsNullOrUndefined(accountName) || !ctx.helper.checkDataIsNullOrUndefined(passWord))  {ctx.status = 200; return;}
+    const result = await ctx.model.SysUser.findOne({
+      where:{accountName: accountName, passWord: passWord}
+    });
+    if(!result)
+    {ctx.status = 200;ctx.body = {status:'error',message:'用户名或者密码错误'}; return;}
+    else{
+      const token  = ctx.helper.signIn(result.id);
+      ctx.body = {status:'success',token:token};
+    }
   }
 }
 
--- sequelize/app/extend/helper.js
+++ sequelize/app/extend/helper.js
@@ -1,9 +1,22 @@
-'use strict';
+"use strict";
 
 module.exports = {
+  secretOrPrivateKey: "cehuiyuanzhaobiaoxiangmu", // 这是加密的key（密钥）
   parseInt(string) {
-    if (typeof string === 'number') return string;
+    if (typeof string === "number") return string;
     if (!string) return string;
     return parseInt(string) || 0;
   },
+  checkDataIsNullOrUndefined(data) {
+    return data ? true : false;
+  },
+  signIn(content) {
+    const token = jwt.sign(content, this.secretOrPrivateKey);
+    return token;
+  },
+  decode(req) {
+    const token =
+      req.body.token || req.query.token || req.headers["authorization"]; // 从body或query或者header中获取token
+    return jwt.decode(token);
+  }
 };
